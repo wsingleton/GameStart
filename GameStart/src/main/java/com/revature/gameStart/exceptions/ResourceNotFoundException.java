d4c5d03f4882b670e0ac0734c62d9bcf54989c15
wezley <wezley.singleton@gmail.com>
2021-03-08 19:25:22 -0500

    stripping away the unneeded stuff for testing deployment

--- GameStart/src/main/java/com/revature/gameStart/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.gameStart.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException() {
-        super("No resource(s) found");
-    }
-}
