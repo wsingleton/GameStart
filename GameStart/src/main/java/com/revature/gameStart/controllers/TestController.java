ab0a51e14c41aa6b4f26775146e24a38eb1c5961
wezley <wezley.singleton@gmail.com>
2021-03-08 19:04:25 -0500

    added test endpoint for verifying deployment

---
+++ GameStart/src/main/java/com/revature/gameStart/controllers/TestController.java
@@ -0,0 +1,31 @@
+package com.revature.gameStart.controllers;
+
+
+import com.revature.gameStart.models.Game;
+import com.revature.gameStart.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.MediaType;
+import java.util.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    //Constructors --------------------------------------------------
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    //Get -----------------------------------------------------------
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String test(){
+        return "deployment successful!";
+    }
+
+}
